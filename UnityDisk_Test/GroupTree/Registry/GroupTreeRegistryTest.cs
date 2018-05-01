4cbd8a77c046bc494f3f7317321bdbdfe7ea9142
shazhko-artem <shazhko.artem@gmail.com>
2018-05-01 12:55:24 +0300

    add test for GroupTreeRegistry.Delete

--- UnityDisk_Test/GroupTree/Registry/GroupTreeRegistryTest.cs
+++ UnityDisk_Test/GroupTree/Registry/GroupTreeRegistryTest.cs
@@ -141,5 +141,67 @@ namespace UnityDisk_Test.GroupTree.Registry
             Assert.IsTrue(expectedContainer.Equals(rootProjection));
         }
 
+        [TestMethod]
+        public void Can_Delete()
+        {
+            var groupSettingsStub = new GroupSettingsContainer()
+            {
+                Name = "Root",
+                Items = new List<GroupSettingsItem>()
+                {
+                    new GroupSettingsContainer()
+                    {
+                        Name = "Container1",
+                        Items = new List<GroupSettingsItem>()
+                        {
+                            new GroupSettingsContainer() {Name = "Container2"},
+                            new GroupSettingsGroup() {Name = "Group2"}
+                        },
+                        IsActive = true
+                    },
+                    new GroupSettingsGroup() {Name = "Group", Items = new List<string>() {"Account1"}}
+                }
+            };
+            SpaceSize expectedSize = new SpaceSize() { TotalSize = 100, FreelSize = 30, UsedSize = 70 };
+
+            var expectedContainer = new ContainerProjection(new Container()
+            {
+                Name = "Root",
+                Size = expectedSize,
+                Items = new List<IGroupTreeItem>()
+                {
+                    new Container()
+                    {
+                        Name = "Container1",
+                        IsActive = true,
+                        Items = new List<IGroupTreeItem>()
+                        {
+                            new Container() {Name = "Container2", Size = new SpaceSize()},
+                        },
+                        Size = new SpaceSize()
+                    },
+                    new Group() {Name = "Group", Size = expectedSize, Items = new List<IAccountProjection>(){_accountStub.Object}}
+                }
+            });
+            _mockService.Setup(settings => settings.LoadGroupTree()).Returns(groupSettingsStub);
+            _accountRegistryStub.Setup(accountRegistry => accountRegistry.Find("Account1")).Returns(_accountStub.Object);
+            _accountStub.SetupGet(accountProjection => accountProjection.Size).Returns(() => expectedSize);
+            _accountStub.SetupGet(accountProjection => accountProjection.Login).Returns(() => "Account1");
+
+            _groupContainerStub = new UnityContainer();
+            _groupContainerStub.RegisterInstance<IGroupSettings>(_mockService.Object);
+            _groupContainerStub.RegisterInstance<IAccountRegistry>(_accountRegistryStub.Object);
+            _groupContainerStub.RegisterType<IContainer, Container>(new InjectionConstructor());
+            _groupContainerStub.RegisterType<IGroup, Group>(new InjectionConstructor());
+
+            GroupTreeRegistry registry = new GroupTreeRegistry(_groupContainerStub);
+
+            registry.Initialization();
+            registry.Delete(new List<string>(){ "Container1" }, "Group2", GroupTreeTypeEnum.Group);
+
+            IContainerProjection rootProjection = registry.GetContainerProjection(new List<string>(), null);
+            Assert.IsTrue(expectedContainer.Equals(rootProjection));
+        }
+
     }
 }
