0f8138ff348b378b82b97efe8afcce4511133e25
shazhko-artem <shazhko.artem@gmail.com>
2018-05-07 18:29:31 +0300

    add remote initialization for Unit test

---
+++ UnityDisk/RemoteInitialization.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.StartScreen;
+using Microsoft.Identity.Client;
+
+namespace UnityDisk
+{
+    public sealed class RemoteInitialization
+    {
+        public static async Task Start()
+        {
+            var test = UnityDisk.App.PublicClientApp.Users;
+            try
+            {
+                    await UnityDisk.App.PublicClientApp.AcquireTokenSilentAsync(UnityDisk.App.Scopes,
+                        UnityDisk.App.PublicClientApp.Users.FirstOrDefault());
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+                try
+                {
+                     await UnityDisk.App.PublicClientApp.AcquireTokenAsync(UnityDisk.App.Scopes);
+                }
+                catch (MsalException msalex)
+                {
+                    Debug.WriteLine($"Error Acquiring Token:{System.Environment.NewLine}{msalex}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}");
+            }
+        }
+    }
+}
