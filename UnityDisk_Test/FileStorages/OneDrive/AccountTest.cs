80cda9497d35e490c5d06a1d7e4d5817c018d815
shazhko-artem <shazhko.artem@gmail.com>
2018-05-07 16:27:06 +0300

    add test

--- UnityDisk_Test/FileStorages/OneDrive/AccountTest.cs
+++ UnityDisk_Test/FileStorages/OneDrive/AccountTest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Identity.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UnityDisk.Accounts;
 using Account = UnityDisk.FileStorages.OneDrive.Account;
@@ -12,11 +14,40 @@ namespace UnityDisk_Test.FileStorages.OneDrive
     [TestClass]
     public class AccountTest
     {
+        private string _login = "shazhko.artem@gmail.com";
+        private AuthenticationResult _authenticationResult ;
+        [TestInitialize]
+        public async Task BeforeEachTest()
+        {
+            try
+            {
+                _authenticationResult =
+                    await UnityDisk.App.PublicClientApp.AcquireTokenSilentAsync(UnityDisk.App.Scopes,
+                        UnityDisk.App.PublicClientApp.Users.FirstOrDefault());
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+                try
+                {
+                    _authenticationResult = await UnityDisk.App.PublicClientApp.AcquireTokenAsync(UnityDisk.App.Scopes);
+                }
+                catch (MsalException msalex)
+                {
+                    Debug.WriteLine($"Error Acquiring Token:{System.Environment.NewLine}{msalex}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}");
+            }
+        }
+
         [TestMethod]
         public async Task Can_SignIn()
         {
             Account account = new Account();
-            await account.SignIn("EwA4A8l6BAAURSN/FHlDW5xN74t6GzbtsBBeBUYAAVFhctzMLMe1AjWlU8lCac9ek38DK/aT+pktRoGXigkgG9hFdfRITX0ypWOgtKAzyo5OnFNvovc2/ehyBnf4eg6olJ2c+l70nKvbkfRNz/9YfyqIp5E5unVi/BGtq4/O3Ik9Pqj4TyoTShya7S2rsiemSz7S/2f9kj539+yBQRFiFpzqc7bxPTSnwzCelkr1wlAt991qY6QDKggK3BqTGCnVKwDQVKnzImF9jfNfxstzb5BT/wetsXPTy8r0/UXFuleodyWcsNvGrMjeheBU8x/UQ2Et7YBCY9NECMGWuv9BCRLjRNVgWO1jERVS+f47Cm4Ld0423N1b1urmsjFR7jADZgAACOaey3JEzyBhCAIUapd2xHGt0Y5ju168M9zU6HSujj0vPrsb2UnYuwowDobNHnF3q7bced7ZPM5WypB2dcq1z7fJQ8uksIhxYlnf9OKgaYzPrcnHUBK2btMR0jkd+GhN7GdcuCB8Hk4xo4le1VA9EGW7KSdNz77HWxe+6LYnS8jXOrg/5Xl74lXO3sknkUy22x4ueeAnSENC+iAgXt4m2Dq92fSSIXVdttQ7DzE54WNpY/ySPZF7Hw+zqFAmnEWFDcqD+3LbkuKlDlGSSaYzql5JQlh6nePkjZiMbV0O1WJvWxqDNJt02RRG8GkTGAabkwqvmA9ZS0ELUvuELUhPE1jIbjGR+xUSf+zFkk9zznKbu3pQuYSXhMcJko41CX6vE7Pb71cIhYoeFOlqIeua4vg341QmePIFyvG3ZJ7d7D4fUiN+XlGzJJsta55UxHjy1hAAVdcg5tRtVPTXnTPpMwQ01S5k1UCZT+O/P5B5EJMd0nkwwDXJyibyR+F+QXiH6rJGw/ZOJyDJgTKvCR/lkTWOd0JJkOdbm/oby8rB8cMlC1pQ/LbrMrkiPv424WGaw0pssW4zWDg47WVhCXGn2n4T9RdBMJNdi8PFmT47XAHvjRUouKaPL7oXPXBwGql8kKLPXA7Uy7yYEA1TM3XvSBX8/jq9tNNg7tZF5H8DQ45tZDlHGcoz6I8Rwfr3oPnUh5rSPQI=");
+            await account.SignIn(_login);
             Assert.IsNotNull(account.Size);
             Assert.IsTrue(account.Size.TotalSize > 0);
             Assert.IsTrue(account.Size.UsedSize > 0);
@@ -24,13 +55,14 @@ namespace UnityDisk_Test.FileStorages.OneDrive
             Assert.IsNotNull(account.Login);
             Assert.IsNotNull(account.Id);
             Assert.IsNotNull(account.Token);
-            Assert.AreEqual(account.Status,ConnectionStatusEnum.Connected);
+            Assert.AreEqual(account.Status, ConnectionStatusEnum.Connected);
         }
+
         [TestMethod]
         public async Task Can_Update()
         {
             Account account = new Account();
-            await account.SignIn("EwA4A8l6BAAURSN/FHlDW5xN74t6GzbtsBBeBUYAAVFhctzMLMe1AjWlU8lCac9ek38DK/aT+pktRoGXigkgG9hFdfRITX0ypWOgtKAzyo5OnFNvovc2/ehyBnf4eg6olJ2c+l70nKvbkfRNz/9YfyqIp5E5unVi/BGtq4/O3Ik9Pqj4TyoTShya7S2rsiemSz7S/2f9kj539+yBQRFiFpzqc7bxPTSnwzCelkr1wlAt991qY6QDKggK3BqTGCnVKwDQVKnzImF9jfNfxstzb5BT/wetsXPTy8r0/UXFuleodyWcsNvGrMjeheBU8x/UQ2Et7YBCY9NECMGWuv9BCRLjRNVgWO1jERVS+f47Cm4Ld0423N1b1urmsjFR7jADZgAACOaey3JEzyBhCAIUapd2xHGt0Y5ju168M9zU6HSujj0vPrsb2UnYuwowDobNHnF3q7bced7ZPM5WypB2dcq1z7fJQ8uksIhxYlnf9OKgaYzPrcnHUBK2btMR0jkd+GhN7GdcuCB8Hk4xo4le1VA9EGW7KSdNz77HWxe+6LYnS8jXOrg/5Xl74lXO3sknkUy22x4ueeAnSENC+iAgXt4m2Dq92fSSIXVdttQ7DzE54WNpY/ySPZF7Hw+zqFAmnEWFDcqD+3LbkuKlDlGSSaYzql5JQlh6nePkjZiMbV0O1WJvWxqDNJt02RRG8GkTGAabkwqvmA9ZS0ELUvuELUhPE1jIbjGR+xUSf+zFkk9zznKbu3pQuYSXhMcJko41CX6vE7Pb71cIhYoeFOlqIeua4vg341QmePIFyvG3ZJ7d7D4fUiN+XlGzJJsta55UxHjy1hAAVdcg5tRtVPTXnTPpMwQ01S5k1UCZT+O/P5B5EJMd0nkwwDXJyibyR+F+QXiH6rJGw/ZOJyDJgTKvCR/lkTWOd0JJkOdbm/oby8rB8cMlC1pQ/LbrMrkiPv424WGaw0pssW4zWDg47WVhCXGn2n4T9RdBMJNdi8PFmT47XAHvjRUouKaPL7oXPXBwGql8kKLPXA7Uy7yYEA1TM3XvSBX8/jq9tNNg7tZF5H8DQ45tZDlHGcoz6I8Rwfr3oPnUh5rSPQI=");
+            await account.SignIn(_login);
             account.Size=null;
             await account.Update();
             Assert.IsNotNull(account.Size);
---
+++ UnityDisk_Test/FileStorages/OneDrive/FileStorageFolderTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityDisk.Accounts;
+using UnityDisk.Accounts.Registry;
+using UnityDisk.FileStorages.OneDrive;
+namespace UnityDisk_Test.FileStorages.OneDrive
+{
+    [TestClass]
+    public class FileStorageFolderTest
+    {
+        [TestMethod]
+        public async Task Can_SignIn()
+        {
+            UnityDisk.FileStorages.OneDrive.Account account = new UnityDisk.FileStorages.OneDrive.Account();
+            await account.SignIn("shazhko.artem@gmail.com");
+            FileStorageFolder folder=new FileStorageFolder(new AccountProjection(
+                new UnityDisk.Accounts.Account(account)));
+            
+            Assert.IsNotNull(account.Size);
+            Assert.IsTrue(account.Size.TotalSize > 0);
+            Assert.IsTrue(account.Size.UsedSize > 0);
+            Assert.IsTrue(account.Size.FreelSize > 0);
+            Assert.IsNotNull(account.Login);
+            Assert.IsNotNull(account.Id);
+            Assert.IsNotNull(account.Token);
+            Assert.AreEqual(account.Status, ConnectionStatusEnum.Connected);
+        }
+    }
+}
